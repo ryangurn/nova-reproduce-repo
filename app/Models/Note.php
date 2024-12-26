81051da59de4e068620e197d02eebedaf755bd85
ryangurn <ryangurnick@gmail.com>
2024-12-26 13:38:00 -0800

    Adding code to support #6636

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Note extends Model
 {
-    //
+    public function related(): MorphTo
+    {
+        return $this->morphTo("related");
+    }
 }
